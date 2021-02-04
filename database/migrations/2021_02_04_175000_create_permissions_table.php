d9db641cabab7078a175d429bf509ac94a470868
KLag-SF <k.lagopus.sf19@gmail.com>
2021-02-05 04:08:28 +0900

    Make migration files

---
+++ database/migrations/2021_02_04_175000_create_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->foreignId('user_id');
+            $table->foreignId('group_id');
+            $table->integer('permission_level');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
