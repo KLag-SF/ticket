74e9e6e8f109eee1997e1281a907f6f0f956b715
KLag-SF <k.lagopus.sf19@gmail.com>
2021-02-09 04:33:11 +0900

    for test use

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,4 +1,26 @@
-{{$user->name}}
-@foreach($groups as $group)
-    {{$group->name}}
-@endforeach
+@extends('layouts/app')
+
+@section('content')
+<div class="user_name">
+    Name:{{$user->name}}
+</div>
+<div>
+    Groups which you are belonging
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Permission</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($groups as $group)
+            <tr>
+                <td>{{$group->group_name}}</td>
+                <td>{{$group->role_name}}
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
