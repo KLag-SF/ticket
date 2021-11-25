d5020a8c9b84d0c37e3c925ad47e01b29d0bb817
KLag-SF <k.lagopus.sf19@gmail.com>
2021-11-25 01:11:22 +0000

    Add method to check user's permission

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,29 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\Permission;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    // Check if the user has permission to access group whose id matches $groupId
+    protected function getPermissionLevel($userId, $groupId){
+        try{
+            // firstOrFail() method returns a exception when nothing has found 
+            $permission = Permission::where([
+                    ['user_id', '=', $userId],
+                    ['group_id', '=', $groupId],
+                ])
+                ->firstOrFail();
+            return $permission->permission_level;
+        }catch(ModelNotFoundException $e){
+            // When catch a exception, user doesn't have a permission
+            Log::debug($e);
+            return -1;
+        }
+    }
 }
