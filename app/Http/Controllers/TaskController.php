8db4b0edc007cb1a41c809192cff9c917fbdfec4
KLag-SF <k.lagopus.sf19@gmail.com>
2021-11-26 15:06:26 +0900

    Add Update and Delete method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,48 +2,57 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Group;
-use App\Models\Permission;
 use App\Models\Task;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\App;
 
 class TaskController extends Controller
 {
     public function create($id)
     {
-        return view('task.form', ['id'=>$id]);
+        $task = new Task();
+        return view('task.create', compact('id', 'task'));
+    }
+
+    public function edit($id){
+        try {
+            $task = Task::findOrFail($id);
+        } catch (ModelNotFoundException $e){
+            App::abort(404);
+        }
+        $lv = $this->getPermissionLevel(Auth::user(), $task->group_id);
+        if (($lv <= 2 && $lv > 0) || (Auth::id() == $task->user_id)){
+            return view('task.update', compact('task'));
+        } else {
+            App::abort(403);
+        }
     }
 
     public function store(Request $request, $id)
     {
-        $userId = Auth::id();
-        $groupId = $id;
-
         //  If the user does not have permission,
         // refuse a request with 403
-        try{
-            Permission::where('user_id', $userId)
-                        ->where('group_id', $groupId)
-                        ->firstOrFail();
-        }catch(Exception $e){
-            return view('error.forbidden');
+        if ($this->getPermissionLevel(Auth::id(), $id) < 0){
+            App::abort(403);
         }
 
         //parameters...
         $task = new Task();
-        $task->user_id = $userId;
-        $task->group_id = $groupId;
+        $task->user_id = Auth::id();
+        $task->group_id = $id;
         $task->title = $request->title;
         $task->detail = $request->detail;
         $task->limit = $request->limit;
-        $task-> progress = $request->progress;
+        $task->progress = $request->progress;
         $task->save();
 
         // Redirect to group page
-        return redirect('/group/'.$groupId);
+        return redirect('/group/'.$id);
     }
+
     public function show(Request $request, $id){
         try{
             $task = Task::findOrFail($id);
@@ -53,5 +62,44 @@ class TaskController extends Controller
         }
         // Get the group's task list
         return view('task.index', compact('task'));
+    }
+
+    public function update(Request $request, $id){
+        try {
+            $task = Task::findOrFail($id);
+        } catch (ModelNotFoundException $e){
+            App::abort(404);
+        }
+        $lv = $this->getPermissionLevel(Auth::user(), $task->group_id);
+        // The owner of tasks or Administrators/Managers can edit them
+        if (($lv <= 2 && $lv > 0) || (Auth::id() == $task->user_id)){
+            $task->user_id = Auth::id();
+            $task->group_id = $id;
+            $task->title = $request->title;
+            $task->detail = $request->detail;
+            $task->limit = $request->limit;
+            $task->progress = $request->progress;
+            $task->save();
+            return redirect('/group/'.$task->group_id);
+        } else {
+            App::abort(403);
+        }
+    }
+
+    public function delete($id){
+        try {
+            $task = Task::findOrFail($id);
+        } catch (ModelNotFoundException $e){
+            App::abort(404);
+        }
+        $group_id = $task->group_id;
+        $lv = $this->getPermissionLevel(Auth::user(), $group_id);
+
+        if (($lv <= 2 && $lv > 0) || (Auth::id() == $task->user_id)){
+            $task->delete();
+            return redirect('/group/'.$group_id);
+        } else {
+            App::abort(403);
+        }
     }
 }
