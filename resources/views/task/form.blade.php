c2268c7e9e738d5e851ca9db175d83c0b94bd5f0
KLag-SF <k.lagopus.sf19@gmail.com>
2021-11-26 15:04:25 +0900

    create update form

--- resources/views/task/form.blade.php
+++ resources/views/task/form.blade.php
@@ -6,34 +6,46 @@
             New ticket
         </div>
         <div class="card-body">
-            <form action="/group/{{$id}}/ticket" method="POST">
+            @if($target == 'update')
+                <form action="/task/{{$task->id}}" method="POST">
+                    <input name="_method" value="PUT" type="hidden">
+            @elseif($target == 'create')
+                <form action="/group/{{$id}}/ticket" method="POST">
+            @endif
                 @csrf
                 <div class="form-group">
                     <label for="title">
                         Task Title
                     </label>
-                    <input name="title" class="form-control" type="text"/>
+                    <input name="title" class="form-control" type="text" value="{{$task->id}}"/>
                 </div>
                 <div>
                     <label for="detail">
                         Detail
                     </label>
-                    <textarea name="detail"></textarea>
+                    <textarea name="detail">{{$task->detail}}</textarea>
                 </div>
                 <div>
                     <label for="limit">
                         Limit
                     </label>
-                    <input type="date" name="limit"/>
+                    <input type="date" name="limit" value="{{$task->limit}}"/>
                 </div>
                 <div>
                     <label for="progress">
                         Progress
                     </label>
-                    <input type="number" name="progress"/>
+                    <input type="number" name="progress" value="{{$task->progress}}"/>
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" class="btn btn-success">Submit</button>
             </form>
+            @if($target == "update")
+                <form action="/task/{{$task->id}}" method="POST">
+                    @csrf
+                    <input type="hidden" name="_method" value="DELETE">
+                    <button class="btn btn-danger" type="submit">Delete this task</button>
+                </form>
+            @endif
         </div>
     </div>
 @endsection
